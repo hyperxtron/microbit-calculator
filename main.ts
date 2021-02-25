59d1e6d420813ffda86fa53cef6c8cf40a0df6fe
hyperxtron <64241054+hyperxtron@users.noreply.github.com>
2021-02-25 16:26:51 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    if (Switch == 0) {
+        Switch += 1
+        basic.showNumber(Number_1)
+    }
+    if (Switch) {
+        Number_2 += 1
+        basic.showNumber(Number_2)
+    }
+})
+let Number_2 = 0
+let Switch = 0
+let Number_1 = 0
+Number_1 = 0
+Number_1 = 0
+Switch = 0
